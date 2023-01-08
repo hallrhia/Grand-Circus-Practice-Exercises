87ff59177258a2ee5ca2a8ce3eabe698b03a03b3
Rhianon Hall <hallrhia@gmail.com>
2023-01-08 14:22:02 -0500

    "updates"

---
+++ Counting/script.js
@@ -0,0 +1,33 @@
+for (let i = 1; i <= 10; i++){
+    console.log(i);
+}
+
+for (let i = 10; i >= 1; i--){
+    console.log(i);
+}
+
+let i = 0; 
+while (i < 10){
+    i++;
+    console.log(i);
+}
+
+let a = 11;
+while (a > 1){
+    a--;
+    console.log(a);
+}
+
+let b = 0;
+do {
+    b = b + 1;
+    console.log(b);
+}
+while (b < 10);
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+for (number of numbers){
+    console.log(number);
+}
+
